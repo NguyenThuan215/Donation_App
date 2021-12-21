6742d783a6f5dfcfca9e0a6ac086efcec61a022e
unknown <ngthuanbb1102@gmail.com>
2021-12-21 14:07:25 +0700

    ex1

---
+++ app/src/main/java/com/example/donation_app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.donation_app;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+}
