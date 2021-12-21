91029ca42976701ea6e5e3ea8e46a08deb30ff92
unknown <ngthuanbb1102@gmail.com>
2021-12-21 17:39:05 +0700

    ex6

--- app/src/main/java/com/example/donation_app/MainActivity.java
+++ app/src/main/java/com/example/donation_app/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 
 import android.widget.EditText;
 import android.widget.NumberPicker;
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean targetAchieved = false;
 
 
-    public void donateButtonPressed (View view)
+    public void donateButtonPressed(View view)
     {
         String method = paymentMethod.getCheckedRadioButtonId() == R.id.PayPal ? "PayPal" : "Direct";
 
@@ -107,14 +108,12 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.menuReport : startActivity (new Intent(this, Report.class));
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
